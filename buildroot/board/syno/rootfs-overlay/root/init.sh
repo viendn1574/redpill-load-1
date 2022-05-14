3df27d49fa2f6398dfcd981148d44f61ddd25709
dogodefi <82947946+dogodefi@users.noreply.github.com>
2022-05-14 19:21:36 +0800

    Update init.sh

--- buildroot/board/syno/rootfs-overlay/root/init.sh
+++ buildroot/board/syno/rootfs-overlay/root/init.sh
@@ -9,7 +9,7 @@ wait_time=30
 time_counter=0
 while [ "${usbfs}" = "" ] && [ $time_counter -lt $wait_time ]; do
   sleep 1
-  usbfs=$(fdisk -l /dev/sd* | grep '128 MB' | awk '{print $2}' | awk -F ':' '{print $1}')
+  usbfs=$(fdisk -l /dev/sd* | grep -E '128 MB|160 MB' | awk '{print $2}' | awk -F ':' '{print $1}')
   echo "Still waiting for boot device (waited $((time_counter=time_counter+1)) of ${wait_time} seconds)"
 done
 mount ${usbfs}1 /temp1
